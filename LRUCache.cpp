57a207bf5c86f79610f92e3d4376e009e7f5272a
David Godfrey <david.godfrey99@gmail.com>
2016-09-17 17:21:08 -0700

    Implemented CRTP Cache
    Significant changes were made such as templatizing LRUCache, utilizing CRTP in the Cache base class in order to use perfect fowarding inside of LRUCache::set() and modularizing the linked list semantics inside of a DoublyLinkedList class whose header is included.

--- LRUCache.cpp
+++ LRUCache.cpp
@@ -1,137 +1,123 @@
 #include <map>
+#include <utility>
+#include "DoublyLinkedList.hpp"
 
-struct Node {
-   Node* next;
-   Node* prev;
-   int value;
-   int key;
-   Node(Node* p, Node* n, int k, int val) : prev(p), next(n), key(k), value(val) {};
-   Node(int k, int val) : prev(NULL), next(NULL), key(k), value(val) {};
+namespace detail {
+template<class T, class U>
+struct MappedItem : std::pair<T, U> {
+    using Base = std::pair<T, U>;
+    using Base::Base;
+    T& key()   { return this->first; }
+    U& value() { return this->second; }
 };
 
-class Cache {
-protected: 
-   std::map<int, Node*> mp; // map the key to the node in the linked list
-   int cp;  // capacity
-   Node* tail; // double linked list tail pointer
-   Node* head; // double linked list head pointer
-   virtual void set(int, int) = 0; // set function
-   virtual int get(int) = 0; // get function
-};
-
-class LRUCache : public Cache {
+class ICache {
 public:
+    virtual ~ICache() = default;
+};
+
+template<class Derived, class T>
+class Cache;
+
+template<class Derived, class K, class V>
+class Cache<Derived, MappedItem<K, V>> : public ICache {
+    using T = MappedItem<K, V>;
+protected:
+   using Key = K;
+   using Value = V;
+   using Node = typename DoublyLinkedList<T>::Node;
+   // set function
+   template<class Vi>
+   void set(Key const& key, Vi&& value) {
+       static_cast<Derived&>(*this).set(key, std::forward<Vi>(value));
+   }
+   // get function
+   Value get(Key const& key) {
+       return static_cast<Derived&>(*this).get(key);
+   }
+   std::map<Key, Node*> mp; // map the key to the node in the linked list
+   size_t cp;  // capacity
+   DoublyLinkedList<T> list;
+};
+
+// Concrete derived class
+// Note: Use of this-> required throughout data member access as Cache<T> is a dependent base class
+template<class T>
+class LRUCache : public Cache<LRUCache<T>, T> {
+public:
+    using Node = typename Cache<LRUCache<T>, T>::Node;
+    using Key = typename Cache<LRUCache<T>, T>::Key;
+    using Value = typename Cache<LRUCache<T>, T>::Value;
   // initializes the cache with given capacity
   LRUCache(size_t capacity);
   // updates/sets element with key value pair
-  void set(int, int);
+  template<class Vi>
+  void set(Key const&, Vi&& u);
   // retrieves element with given key (returns -1 if not found)
-  int get(int);
+  Value get(Key const&);
   // size of the cache
   size_t size() const { return m_size; }
 private:
-  // appends a node to the cache
-  void Push(Node*);
-  // appends a node to the cache
-  void Push(int, int);
-  // moves a node to the begining of the list
-  void Prepend(Node*);
-  // creates a node and moves it to the beginning of the list
-  void Prepend(int, int);
-  // creates a new node
-  Node* NewNode(int, int);
+    Node* head() { return this->list.head; }
+    Node* tail() { return this->list.tail; }
   // capacity and current size of the cache
   size_t m_size;
 };
 
-LRUCache::LRUCache(size_t capacity)
+template<class T>
+LRUCache<T>::LRUCache(size_t capacity)
   : m_size(0)
 {
     // set capacity
-    cp = capacity;
-    // create empty cache
-    head = tail = NULL;
-}
-
-// creates a new node
-Node* LRUCache::NewNode(int key, int value) {
-  return new Node(key, value);
-}
-
-// pushes a new node onto the cache
-void LRUCache::Push(int key, int value) {
-  Push(NewNode(key, value));
-}
-
-// prepends a node
-void LRUCache::Prepend(int key, int value) {
-  Prepend(NewNode(key, value));
-}
-// prepends a node
-void LRUCache::Prepend(Node* obj) {
-  if (obj->next) obj->next->prev = obj->prev; // update prev node of obj->next
-  obj->prev->next = obj->next; // remove obj from node list
-  obj->next = head; // append obj to head
-  if (head) head->prev = obj; // update prev node of head
-  head = obj; // update head (now most recently used)
-  // map key to obj
-  mp[obj->key] = obj;
-}
-
-// pushes a new node onto the cache
-void LRUCache::Push(Node* obj) {
-  // if the list is empty
-  if (head == NULL)
-    head = obj;
-  else
-    tail->next = obj;
-  
-  // set previous node and tail
-  obj->prev = tail;
-  tail = obj;
-  
-  // map key to new node
-  mp[obj->key] = obj;
+    this->cp = capacity;
 }
 
 // if key is present and cache length exceeds capacity, move node to beginning of the list (now most recently use)
 // otherwise, remove the least recently used node and prepend new node to beginning
-void LRUCache::set(int key, int value) {
+template<class T>
+template<class Vi>
+void LRUCache<T>::set(Key const& key, Vi&& value) {
   Node* target;
   // if there is a node with that key set target as that node
   if (get(key) != -1) {
-    target = mp[key];
+    target = this->mp[key];
   }
   else {
     // if adding a new node does not exceed capacity, set as NULL to indicate that,
     // otherwise set as tail in order to replace the head
-    target = ((m_size+1) <= cp) ? NULL : tail;
+    target = ((m_size+1) <= this->cp) ? NULL : tail();
   }
    
   // if list is empty (or adding a new node does NOT exceed capacity), append a new node
   if (target == NULL) {
-    Push(key, value);
+    this->list.Push(T(key, value));
+    this->mp[key] = head();
     m_size++;
   }
   // if the target is the head, simply update its value
-  else if (target == head) {
-      head->value = value;
+  else if (target == head()) {
+      head()->data.value() = std::forward<Vi>(value);
   }
   // otherwise update the value of target and set it as the most recently used
   else {
-    mp[target->key] = NULL; // "remove" node
-    target->value = value; // update value
-    target->key = key; // update key
+    this->mp[target->data.key()] = NULL; // "remove" node
+    target->data.value() = std::forward<Vi>(value); // update value
+    target->data.key() = key; // update key
     
     // move node to the beginning of the list
-    Prepend(target);
+    this->list.Prepend(target);
   }
 }
 
 // returns the value mapped to the key or -1 if not found
-int LRUCache::get(int key) {
+template<class T>
+auto LRUCache<T>::get(Key const& key) -> Value {
   // mp[key] is NULL only if it has been removed from the cache
-  return (mp.find(key) != mp.end()) && mp[key] != NULL
-    ? mp[key]->value
+  return (this->mp.find(key) != this->mp.end()) && this->mp[key] != NULL
+    ? this->mp[key]->data.value()
     : -1;
 }
+}
+
+template<class T>
+using LRUCache = detail::LRUCache<detail::MappedItem<int, T>>;
